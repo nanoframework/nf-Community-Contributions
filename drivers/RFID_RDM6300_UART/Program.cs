7ad8c0eef56d5541ee4d64d4c3a376da047707cc
Valon M Hoti <valon.hoti@gmail.com>
2020-05-05 08:33:46 +0200

    RFID_RDM6300_UART

---
+++ drivers/RFID_RDM6300_UART/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+
+namespace nf.RDM6300
+{
+    public class Program
+    {
+        static Rdm630 rfid;
+        static int cnt = 0;
+        public static void Main()
+        {
+
+            rfid = new Rdm630("COM6");
+            rfid.DataReceived += Rfid_DataReceived;
+
+         
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void Rfid_DataReceived(uint data1, uint data2, DateTime time)
+        {
+           
+            Console.WriteLine(rfid.Tag);
+
+        }
+    }
+}
