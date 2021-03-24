c521439302e1aa9b1728d4a617d56697eb8a22b1
Vyacheslav Golovanov <vgolovanov@gmail.com>
2021-03-24 14:52:11 +0200

    BME280 Driver

---
+++ drivers/BME280/DeviceInitialisationException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace MBN
+{
+    /// <summary>
+    /// Exception thrown when a new instance of a driver can't be created. It may be because of too short delays or bad commands sent to the device.
+    /// </summary>
+    [Serializable]
+    public class DeviceInitialisationException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceInitialisationException"/> class.
+        /// </summary>
+        public DeviceInitialisationException() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceInitialisationException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public DeviceInitialisationException(String message) : base(message) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceInitialisationException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public DeviceInitialisationException(String message, Exception innerException) : base(message, innerException) { }
+
+        /// <summary>
+        /// Gets the <see cref="T:System.Exception" /> instance that caused the current exception.
+        /// </summary>
+        /// <returns>An instance of Exception that describes the error that caused the current exception. The InnerException property returns the same value as was passed into the constructor, or a null reference (Nothing in Visual Basic) if the inner exception value was not supplied to the constructor. This property is read-only.</returns>
+        public new Exception InnerException { get { return base.InnerException; } }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        /// <PermissionSet><IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" PathDiscovery="*AllFiles*" /></PermissionSet>
+        public override String ToString() => "DeviceInitialisationException : " + base.Message;
+    }
+}
---
+++ drivers/BME280/IHumidity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace MBN
+{
+    /// <summary>
+    /// Interface used for drivers using humidity sensors
+    /// </summary>
+    public interface IHumidity
+    {
+        /// <summary>
+        /// Reads the relative or absolute humidity value from the sensor.
+        /// </summary>
+        /// <returns>A single representing the relative/absolute humidity as read from the sensor, in percentage (%) for relative reading or value in case of absolute reading.</returns>
+        Single ReadHumidity(HumidityMeasurementModes measurementMode = HumidityMeasurementModes.Relative);
+
+        /// <summary>
+        /// Gets the raw data of the humidity value.
+        /// </summary>
+        /// <value>
+        /// Raw data in the range depending on sensor's precision (8/10/12 bits, for example)
+        /// </value>
+        Int32 RawData { get; }
+    }
+}
